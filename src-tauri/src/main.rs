b0361dfc729a1c5f52ccb44f3870ff8944a81bfd
Michael Moore <mscottmoore@protonmail.com>
2024-03-15 15:40:59 -0500

    chore: update dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -26,6 +26,7 @@ mod parser;
 
 pub fn main() {
 	tauri::Builder::default()
+		.plugin(tauri_plugin_shell::init())
 		.plugin(tauri_plugin_dialog::init())
 		.plugin(tauri_plugin_fs::init())
 		.invoke_handler(tauri::generate_handler![
