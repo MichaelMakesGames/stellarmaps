ec1b71868aa4cb6cf0b0670898450897fc53d32b
Michael Moore <mscottmoore@protonmail.com>
2024-09-17 23:01:04 -0500

    chore: bump version

--- src/renderer/src/lib/constants.ts
+++ src/renderer/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const VERSION = '0.12.2';
+export const VERSION = '0.13.0-dev';
 
 export const SYSTEM_FONTS =
 	'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
